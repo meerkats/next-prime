745085b2ae1c8215d86cb067474216352861b61b
Kenrick Wu <kenrick.wu@gmail.com>
2018-04-19 19:40:55 +0800

    Added testing packages for jest and typescript

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+	verbose: true,
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
